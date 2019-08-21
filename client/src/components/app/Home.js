4791f59a3c7e4c80b03c472d5b10d798f684a9b3
Yiling <yiling2kang@gmail.com>
2019-08-20 23:09:41 -0700

    added scroll down for intro section and additional intro details

--- client/src/components/app/Home.js
+++ client/src/components/app/Home.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { createRef } from 'react';
 import Tree from '../../assets/images/Tree';
 import { Next } from '../../assets/icons';
 import Intro from './Intro';
 import '../../stylesheets/app/home.scss';
 
 const Home = () => {
+    const introRef = createRef();
+
+    const scrollToIntro = () => {
+        window.scrollTo({
+            top: introRef.current.offsetTop,
+            left: 0,
+            behavior: 'smooth',
+        });
+    };
+
     return (
         <div>
-            <div className="home">
+            <div className="home" id="home">
                 <div className="tree">
                     <Tree className="tree_svg" pathClass="tree__path" />
                 </div>
                 <div className="nextButton">
-                    <Next
+                    <button
                         className="nextButton_svg"
-                        width="3em"
-                        height="3em"
-                        viewBox="0 0 129 129"
-                    />
+                        type="button"
+                        onClick={() => scrollToIntro()}
+                        onMouseDown={e => e.preventDefault()}
+                    >
+                        <Next width="1.5em" height="1.5em" viewBox="0 0 129 129" />
+                    </button>
                 </div>
             </div>
-            <Intro />
+            <Intro introRef={introRef} />
         </div>
     );
 };
--- client/src/components/app/Intro.js
+++ client/src/components/app/Intro.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../../stylesheets/app/intro.scss';
 
-const Intro = () => {
-    return (
-        <div className="intro">
-            <div className="intro__greeting">hey everyone!</div>
-            <div className="intro__info">
-                I'm Yiling, a full-stack developer and designer, currently based in Vancouver,
-                Canada.
+class Intro extends Component {
+    componentDidMount() {
+        const greeting = document.getElementById('greeting');
+        const info = document.getElementById('info');
+
+        const fadeInOut = element => {
+            element.style.opacity = window.scrollY / 250;
+        };
+
+        window.onscroll = () => {
+            fadeInOut(greeting);
+            fadeInOut(info);
+        };
+    }
+
+    render() {
+        const { introRef } = this.props;
+
+        return (
+            <div className="intro" id="intro" ref={introRef}>
+                <div className="intro__greeting" id="greeting">
+                    hey everyone!
+                </div>
+                <div className="intro__info" id="info">
+                    I&apos;m Yiling, a full-stack developer and designer currently based in
+                    Vancouver, Canada. <br />
+                    <br />I am looking for a <b>4 or 8 month</b> software development co-op or
+                    intership position starting <b>January 2020</b>!
+                </div>
             </div>
-        </div>
-    );
-};
+        );
+    }
+}
 
 export default Intro;
